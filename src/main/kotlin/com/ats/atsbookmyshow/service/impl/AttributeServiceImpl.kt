fb7dcbc1576c7fa18bac5a329259a25c27a94ecf
Souptikgithub1 <souptik.karmakar93@gmail.com>
2021-09-29 22:19:44 +0530

    search Attribute modified

--- src/main/kotlin/com/ats/atsbookmyshow/service/impl/AttributeServiceImpl.kt
+++ src/main/kotlin/com/ats/atsbookmyshow/service/impl/AttributeServiceImpl.kt
@@ -2,6 +2,7 @@ package com.ats.atsbookmyshow.service.impl
 
 import com.ats.atsbookmyshow.domain.Attribute
 import com.ats.atsbookmyshow.service.AttributeService
+import com.ats.atsbookmyshow.utils.StringConstants
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate
 import org.springframework.data.mongodb.core.query.Criteria
 import org.springframework.data.mongodb.core.query.Query
@@ -15,8 +16,8 @@ class AttributeServiceImpl(
 
     override fun search(searchValue: String?, page: Int, pageSize: Int): Flux<Attribute> {
         val query = Query()
-        if (searchValue!=null) {
-            query.addCriteria(Criteria.where("attributeName").regex(searchValue, "i"))
+        if (searchValue != null) {
+            query.addCriteria(Criteria.where(StringConstants.ATTRIBUTE_NAME).regex(searchValue, "i"))
         }
         query.skip(((page-1) * pageSize).toLong())
         query.limit(pageSize)
--- src/main/kotlin/com/ats/atsbookmyshow/utils/StringConstants.kt
+++ src/main/kotlin/com/ats/atsbookmyshow/utils/StringConstants.kt
@@ -3,4 +3,7 @@ package com.ats.atsbookmyshow.utils
 object StringConstants {
     const val PRODUCT_COLLECTION = "product"
     const val CATEGORY_COLLECTION = "category"
+
+    //attributes
+    const val ATTRIBUTE_NAME = "attributeName"
 }
